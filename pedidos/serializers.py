dd7c02f5ad844cfd944b5cc3eaaf54286a9ad3bf
kleyner01 <kleyner01@gmail.com>
2025-02-13 11:24:08 -0400

    Adicionei formatação na data corrta de forma brasileira

--- pedidos/serializers.py
+++ pedidos/serializers.py
@@ -4,6 +4,9 @@ from .models import Pedido
 
 
 class PedidoSerializer(serializers.ModelSerializer):
+    ped_conf_cli_dt_envio = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S")
+    ped_conf_cli_dt_lancto = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S")
+
     class Meta:
         model = Pedido
         fields = '__all__'
