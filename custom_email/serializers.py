1a50b00bf7efd7af569f4478f040f073d1d6e798
kleyner01 <kleyner01@gmail.com>
2025-02-13 12:31:39 -0400

    Descrição das alterações realizadas

--- custom_email/serializers.py
+++ custom_email/serializers.py
@@ -4,6 +4,8 @@ from .models import EmailRegistro
 
 
 class EmailRegistroSerializer(serializers.ModelSerializer):
+    ped_conf_cli_dt_confirmacao = serializers.DateTimeField(format="%d/%m/%Y - Hora: %H:%M:%S")
+
     class Meta:
         model = EmailRegistro
         fields = '__all__'
--- pedidos/serializers.py
+++ pedidos/serializers.py
@@ -4,8 +4,9 @@ from .models import Pedido
 
 
 class PedidoSerializer(serializers.ModelSerializer):
-    ped_conf_cli_dt_envio = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S")
-    ped_conf_cli_dt_lancto = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S")
+    ped_conf_cli_dt_envio = serializers.DateTimeField(format="%d/%m/%Y - Hora: %H:%M:%S")
+    ped_conf_cli_dt_lancto = serializers.DateTimeField(format="%d/%m/%Y - Hora: %H:%M:%S")
+    ped_conf_cli_dt_confirmacao = serializers.DateTimeField(format="%d/%m/%Y - Hora: %H:%M:%S")
 
     class Meta:
         model = Pedido
